ff642550e0cc58548f2d5872af4fcbec6ea3c77c
Jordan Parker <parkerj4321@gmail.com>
2018-08-02 11:39:25 -0500

    Fixed the account balance print

--- Account.js
+++ Account.js
@@ -26,14 +26,14 @@ function main() {
 
     var checking = new Account("Jordan's Checking", 100);
     checking.deposit(20);
-    console.log(checking);
+    console.log(checking.balance());
 
     var checking2 = new Account("Matt's Account", 1000);
     var checking3 = new Account("My account", 0);
     checking2.withdrawal(100);
     checking3.deposit(100);
-    console.log(checking2);
-    console.log(checking3);
+    console.log(checking2.balance());
+    console.log(checking3.balance());
 
 
 
